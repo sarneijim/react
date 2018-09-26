d42beb2dd3e2e2c46e28f8a1e466a610cb0402f1
sara <sarneijim@gmail.com>
2018-09-26 13:06:34 -0400

    SPA part1

--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {  Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
 
-function RenderMenuItem({dish, onClick}){
+function RenderMenuItem({dish}){
   return(
     <Card>
       <CardImg width="100%" src={dish.image} alt={dish.name} />
@@ -17,7 +17,7 @@ const Menu = (props) => {
   const menu = props.dishes.map((dish) => {
     return (
       <div key={dish.id} className="col-12 col-md-5 m-1">
-        <RenderMenuItem dish={dish} onClick={props.onClick} />
+        <RenderMenuItem dish={dish} />
       </div>
     );
   });
