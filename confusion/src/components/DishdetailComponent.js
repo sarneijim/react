f76d47b32d99a2aa54c5de60812c8b2cdfa5d120
sara <sarneijim@gmail.com>
2018-09-26 16:02:06 -0400

    SPA 2

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -1,7 +1,8 @@
 
 import React from 'react';
 import {  Card, CardImg, CardText, CardBody,
-    CardTitle } from 'reactstrap';
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderDish({dish}) {
   return(
@@ -43,12 +44,21 @@ const DishDetail = (props) => {
     return(
       <div className="container">
         <div className="row">
-          <div  className="col-12 col-md-5 m-1">
-            <RenderDish dish={props.dish} />
+          <Breadcrumb>
+            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+          </Breadcrumb>
+          <div className="col-12">
+            <h3>{props.dish.name}</h3>
+            <hr />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12 col-md-5 m-1">
+              <RenderDish dish={props.dish} />
           </div>
           <div className="col-12 col-md-5 m-1">
-            <RenderTitle />
-            <RenderComments comments={props.dish.comments} />
+              <RenderComments comments={props.comments} />
           </div>
         </div>
       </div>
